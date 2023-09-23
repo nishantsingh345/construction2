defa34c28c8da60a899fc90d0f862c5f4d831d6e
Nishant <nishantsingh95340@gmail.com>
2023-09-23 10:59:09 +0530

    modified footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -30,16 +30,6 @@ const Quickinks = [
     title: "Privacy Policy",
     url: "/quicklinks/privacy-policy",
   },
-  {
-    id: 2,
-    title: "Terms and Conditions",
-    url: "/quicklinks/teermsofuse",
-  },
-  {
-    id: 3,
-    title: "Privacy Policy",
-    url: "/quicklinks/privacy-policy",
-  },
 ];
 
 const Usefulllinks = [
@@ -47,16 +37,6 @@ const Usefulllinks = [
     id: 1,
     title: "Home",
     url: "/",
-  },
-  {
-    id: 2,
-    title: "Terms and Conditions",
-    url: "/quicklinks/teermsofuse",
-  },
-  {
-    id: 3,
-    title: "Privacy Policy",
-    url: "/quicklinks/privacy-policy",
   },
   {
     id: 2,
@@ -76,12 +56,12 @@ function Footer() {
         <div className="space-y-6 md:px-8">
           <div className="grid lg:grid-cols-4 space-x-6 space-y-6">
             <div className="w-fit space-y-4 text-justify">
-              <p>Logo</p>
+              <img src="/logo1.jpeg" alt="" />
               <div className="flex flex-col gap-4 ">
                 <p>
-                  Our astrology course at Astrosevatalk.in comes with several practical features to
-                  make learning more practical for all aspiring astrologers. It includes the
-                  following features to make learning smooth and practical.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum culpa dicta
+                  nesciunt tempora nam eligendi repudiandae nisi, possimus commodi quasi maiores
+                  autem dolor fugit. Ipsum necessitatibus veritatis impedit facilis eveniet.
                 </p>
               </div>
             </div>
@@ -132,11 +112,11 @@ function Footer() {
                     info@construction.in
                   </p>
                 </div>
-                <div className="text-justify gap-1 items-start flex">
+                {/* <div className="text-justify gap-1 items-start flex">
                   <FaMap size={25} />
                   Address: SPACIFY Plot 452 First Floor, Sector 19, Pocket 3, Dwarka, New Delhi,
                   Delhi 110075
-                </div>
+                </div> */}
                 <div className="flex gap-4 text-white">
                   <Link href="https://www.facebook.com/profile.php?id=100091654873768">
                     <FaFacebook size={35} />

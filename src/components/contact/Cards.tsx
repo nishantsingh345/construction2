f3d612ad4f3fa3a1e8bf70fc1eb236bcdcc1c7e8
Nishant <nishantsingh95340@gmail.com>
2023-09-26 14:11:04 +0530

    adding icons in contact

--- src/components/contact/Cards.tsx
+++ src/components/contact/Cards.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Section from "../common/Section";
-import { FaAddressCard } from "react-icons/fa";
+import { FaAddressCard, FaPhone } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 type Props = {};
 
@@ -13,15 +14,15 @@ const card = [
   },
   {
     id: 1,
-    icon: <FaAddressCard size={40} />,
-    title: "Registered Address:",
-    text: "Lorem ipsum dolor sit amet",
+    icon: <FaPhone size={40} />,
+    title: "Phone:",
+    text: "+91 800 0016 122",
   },
   {
     id: 1,
-    icon: <FaAddressCard size={40} />,
-    title: "Registered Address:",
-    text: "Lorem ipsum dolor sit amet",
+    icon: <MdEmail size={40} />,
+    title: "Email:",
+    text: "info@constructioncardshelpline.co.uk",
   },
 ];
 
@@ -29,18 +30,18 @@ const Cards = (props: Props) => {
   return (
     <div className="py-10">
       <Section>
-        <div className="grid md:grid-cols-3 gap-4">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
           {card.map((item, index) => (
             <div
               key={index}
-              className="group bg-[#040f28]/70 hover:bg-[#ff5e14] ease-in-out duration-300 rounded-md p-5 mx-auto"
+              className="group bg-[#040f28]/70 hover:bg-[#ff5e14] ease-in-out duration-300 rounded-md px-2 py-5 mx-auto w-80"
             >
               <div className="flex items-center  justify-center ">
                 <div className=" text-white bg-[#040f28] px-6 py-3 rounded-3xl  group-hover:bg-[#ff5e14]">
                   {item?.icon}
                 </div>
               </div>
-              <div className="py-4 bg-[#040f28] px-10 mt-3 text-white rounded-3xl">
+              <div className="py-4 bg-[#040f28] px-3 md:px-6 mt-3 text-white rounded-3xl">
                 <p className="text-center font-bold text-2xl">{item?.title}</p>
                 <p className="text-center pt-3 ">{item?.text}</p>
               </div>

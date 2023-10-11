0e8c0b08ede25b0f15d3e44e1d37a431b4471416
Nishant <nishantsingh95340@gmail.com>
2023-10-11 15:42:35 +0530

    add icons and modified book

--- src/components/home/Book.tsx
+++ src/components/home/Book.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Section from "../common/Section";
 import Link from "next/link";
+import { RiComputerFill } from "react-icons/ri";
+import { GrNotes } from "react-icons/gr";
+import { FaBookReader } from "react-icons/fa";
 
 type Props = {};
 
@@ -9,21 +12,25 @@ const card = [
     id: 1,
     text: "CITB TEST",
     link: "/citb/book-now",
+    icon: <RiComputerFill size={30} />,
   },
   {
     id: 1,
     text: "COURSE",
     link: "/course/book-now",
+    icon: <GrNotes size={30} />,
   },
   {
     id: 1,
     text: "CSCS CARDS",
     link: "/cscscards/book-now",
+    icon: <FaBookReader size={30} />,
   },
   {
     id: 1,
     text: "REVISION MATERIAL",
     link: "/revisnoal-material",
+    icon: <FaBookReader size={30} />,
   },
 ];
 
@@ -40,19 +47,23 @@ const Book = (props: Props) => {
               alt=""
             />
           </div>
-          <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-10">
+          <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-14">
             {card.map((item, index) => (
               <div
                 key={index}
                 className="border-2 rounded-2xl w-[225px] h-[120px] bg-white border-[#ff5e14] relative  px-2 mx-auto"
               >
+                <Link href={item?.link} className="absolute -top-5 left-[50px]">
+                  <div className="bg-white p-2 w-32 h-14 text-[#040f28] rounded-b-2xl border-2 border-[#ff5e14] border-t-2 border-t-[#040f28] flex items-center justify-center">
+                    {item?.icon}
+                  </div>
+                </Link>
                 <div>
                   <p className="flex items-center justify-center text-black font-bold pt-12">
                     {item?.text}
                   </p>
                 </div>
-
-                <Link href={item?.link} className="relative bottom-0 left-14 top-[26px]">
+                <Link href={item?.link} className="absolute -bottom-5 left-[66px]">
                   <button className="bg-[#ff5e14] p-2 text-white rounded-2xl">Book Now</button>
                 </Link>
               </div>

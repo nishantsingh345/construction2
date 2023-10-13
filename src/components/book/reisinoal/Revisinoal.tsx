bc1d5832e91dd54ecaf088177c38ec512ec8de7a
Nishant <nishantsingh95340@gmail.com>
2023-10-13 16:24:41 +0530

    add desgin

--- src/components/book/reisinoal/Revisinoal.tsx
+++ src/components/book/reisinoal/Revisinoal.tsx
@@ -1,48 +1,247 @@
 "use client";
-
 import Section from "@/components/common/Section";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "@/components/ui/use-toast";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import * as z from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
+const items = [
+  {
+    id: "hS&Ebook",
+    label: "HS&E Test for Operatives & Specialists Book",
+  },
+  {
+    id: "hS&Edvd",
+    label: "HS&E Test for Operatives & Specialists DVD",
+  },
+  {
+    id: "safetybook",
+    label: "Health, safety and environment test for managers and professionals Book",
+  },
+  {
+    id: "safetydvd",
+    label: "Health, safety and environment test for managers and professionals DVD",
+  },
+] as const;
+
+const FormSchema = z.object({
+  firstName: z.string().min(2).max(50),
+  lastname: z.string().min(2).max(50),
+  birthdate: z.string().min(2).max(50),
+  number: z.string().min(10).max(50),
+  email: z.string().min(1).max(50),
+  postcode: z.string().optional(),
+  address: z.string().min(1).max(50),
+  insurence: z.string().min(1).max(50),
+  items: z.array(z.string()).refine((value) => value.some((item) => item), {
+    message: "You have to select at least one item.",
+  }),
 });
 
 type Props = {};
 
 const Revisinoal = (props: Props) => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      items: [""],
+    },
   });
 
-  const onSubmit = () => {
-    console.log("form subited");
+  const onSubmit = (values: z.infer<typeof FormSchema>) => {
+    console.log(values);
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      ),
+    });
   };
+
   return (
-    <div>
+    <div className="py-10">
       <Section>
-        <div>
+        <div className="border-slate-700 border-2 p-3 rounded-md">
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>This is your public display name.</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Submit</Button>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-4">
+              <div className="w-full space-y-4 border-2 border-slate-700 p-3 rounded-md">
+                <p className="font-bold space-y-2 text-2xl">Persnoal Details</p>
+                <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+                  <FormField
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                      <FormItem className="w-full space-y-1">
+                        <FormLabel>First Name*</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Name" type="text" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="lastname"
+                    render={({ field }) => (
+                      <FormItem className="w-full space-y-1">
+                        <FormLabel>Last Name*</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Name" type="text" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+                  <FormField
+                    control={form.control}
+                    name="birthdate"
+                    render={({ field }) => (
+                      <FormItem className="w-full space-y-1">
+                        <FormLabel>Birth Of Date*</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Birth of Date" type="date" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="number"
+                    render={({ field }) => (
+                      <FormItem className="w-full space-y-1">
+                        <FormLabel>Mobile*</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Number" type="number" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem className="w-full space-y-1">
+                        <FormLabel>Email*</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Email" type="email" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="postcode"
+                    render={({ field }) => (
+                      <FormItem className="w-full space-y-1">
+                        <FormLabel>Postcode*</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Postcode" type="text" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+                  <FormField
+                    control={form.control}
+                    name="address"
+                    render={({ field }) => (
+                      <FormItem className="w-full space-y-1">
+                        <FormLabel>Address*</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Address" type="text" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="insurence"
+                    render={({ field }) => (
+                      <FormItem className="w-full space-y-1">
+                        <FormLabel>Insurence No.*</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Insurence Number" type="text" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div className="w-full space-y-4 border-2 border-slate-700 p-3 rounded-md">
+                <p className="font-bold space-y-2 text-2xl">Buy Books & DVD</p>
+                <FormField
+                  control={form.control}
+                  name="items"
+                  render={() => (
+                    <FormItem>
+                      {items.map((item) => (
+                        <FormField
+                          key={item.id}
+                          control={form.control}
+                          name="items"
+                          render={({ field }) => {
+                            return (
+                              <FormItem
+                                key={item.id}
+                                className="flex  md:items-center items-start  gap-2 space-y-4 justify-between"
+                              >
+                                <FormLabel className="font-normal">{item.label}</FormLabel>
+                                <FormControl>
+                                  <Checkbox
+                                    checked={field.value?.includes(item.id)}
+                                    onCheckedChange={(checked) => {
+                                      return checked
+                                        ? field.onChange([...field.value, item.id])
+                                        : field.onChange(
+                                            field.value?.filter((value) => value !== item.id)
+                                          );
+                                    }}
+                                  />
+                                </FormControl>
+                              </FormItem>
+                            );
+                          }}
+                        />
+                      ))}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex items-center justify-center">
+                <Button className="bg-[#ff5e14] hover:bg-[#ff5e14]/90" size="lg" type="submit">
+                  PROCEED
+                </Button>
+              </div>
             </form>
           </Form>
         </div>

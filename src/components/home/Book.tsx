1d56bbbcd23dadd0d9881029b99c7a4b3da6281c
Nishant <nishantsingh95340@gmail.com>
2023-10-20 01:15:39 +0530

    add css in book now section

--- src/components/home/Book.tsx
+++ src/components/home/Book.tsx
@@ -47,7 +47,7 @@ const Book = (props: Props) => {
               alt=""
             />
           </div>
-          <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-14">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-14">
             {card.map((item, index) => (
               <div
                 key={index}
